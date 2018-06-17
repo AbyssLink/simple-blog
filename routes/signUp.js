caf548ac7746b415a61e4ff373604522bea58492
程竞泽 <1171146313@qq.com>
2018-06-17 13:53:38 +0800

    完成了登录和注册界面的功能，经测试可行

--- routes/signUp.js
+++ routes/signUp.js
@@ -5,23 +5,23 @@ const express = require('express')
 const router = express.Router()
 
 const UserModel = require('../models/users')
-const checkNotLogin = require('../middlewares/check').checkNotLogin
+const checkNotLogin = require('../middlewares/checkLogin').checkNotLogin
 
 // GET /signup 注册页
 router.get('/', checkNotLogin, function(req, res, next) {
-    res.render('signup')
+    res.render('signUp')
 })
 
 // POST /signup 用户注册
-// 我们使用 express - formidable 处理表单的上传，
-// 表单普通字段挂载到 req.fields 上，表单上传后的文件挂载到 req.files 上
 router.post('/', checkNotLogin, function(req, res, next) {
+    // 我们使用 express - formidable 处理表单的上传，
+    // 表单普通字段挂载到 req.fields 上，表单上传后的文件挂载到 req.files 上
+    // 文件存储在 public/img 目录下
     const name = req.fields.name
     const gender = req.fields.gender
     const bio = req.fields.bio
     const avatar = req.files.avatar.path.split(path.sep).pop()
     let password = req.fields.password
-    const repassword = req.fields.repassword
     const email = req.fields.email
 
     // 校验参数
@@ -30,7 +30,7 @@ router.post('/', checkNotLogin, function(req, res, next) {
             throw new Error('名字请限制在 1-10 个字符')
         }
         if (['male', 'female', 'secret'].indexOf(gender) === -1) {
-            throw new Error('性别只能是 m、f 或 x')
+            throw new Error('性别只能是 male、female 或 secret')
         }
         if (!(bio.length >= 1 && bio.length <= 30)) {
             throw new Error('个人简介请限制在 1-30 个字符')
@@ -48,7 +48,7 @@ router.post('/', checkNotLogin, function(req, res, next) {
         // 注册失败，异步删除上传的头像
         fs.unlink(req.files.avatar.path)
         req.flash('error', e.message)
-        return res.redirect('/signup')
+        return res.redirect('/signUp')
     }
 
     // 明文密码加密
@@ -56,36 +56,45 @@ router.post('/', checkNotLogin, function(req, res, next) {
 
     // 待写入数据库的用户信息
     let user = {
-            name: name,
-            password: password,
-            gender: gender,
-            bio: bio,
-            avatar: avatar,
-            email: email
-        }
-        // 用户信息写入数据库
+        name: name,
+        password: password,
+        gender: gender,
+        bio: bio,
+        avatar: avatar,
+        email: email
+    }
+
+    console.log("username", user.name)
+
+
+    // 用户信息写入数据库
     UserModel.create(user)
-        .then(function(result) {
-            // 此 user 是插入 mongodb 后的值，包含 _id
-            user = result.ops[0]
-                // 删除密码这种敏感信息，将用户信息存入 session
-            delete user.password
-            req.session.user = user
-                // 写入 flash
-            req.flash('success', '注册成功')
-                // 跳转到首页
-            res.redirect('/posts')
-        })
-        .catch(function(e) {
-            // 注册失败，异步删除上传的头像
-            fs.unlink(req.files.avatar.path)
-                // 用户名被占用则跳回注册页，而不是错误页
-            if (e.message.match('duplicate key')) {
-                req.flash('error', '用户名已被占用')
-                return res.redirect('/signup')
-            }
-            next(e)
-        })
+
+    .then(function(result) {
+        // 此 user 是插入 mongodb 后的值，包含 _id
+        user = result.ops[0]
+            // 删除密码这种敏感信息，将用户信息存入 session
+        delete user.password
+        req.session.user = user
+
+        // 写入 flash
+        req.flash('success', '注册成功')
+
+        // 跳转到首页
+        res.redirect('/posts')
+    })
+
+    .catch(function(e) {
+        // 注册失败，异步删除上传的头像
+        fs.unlink(req.files.avatar.path)
+
+        // 用户名被占用则跳回注册页，而不是错误页
+        if (e.message.match('duplicate key')) {
+            req.flash('error', '用户名已被占用')
+            return res.redirect('/posts')
+        }
+        next(e)
+    })
 })
 
 module.exports = router
